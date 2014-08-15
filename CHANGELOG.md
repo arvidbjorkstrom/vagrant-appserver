f565d2ce2859e43980e6f3d609b9951e867f4759
Arvid Björkström <arvid@bjorkstrom.se>
2014-08-15 14:29:56 +0200

    Berkshelf init

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# 0.1.0
+
+Initial release of appserver-template
+
+* Enhancements
+  * an enhancement
+
+* Bug Fixes
+  * a bug fix
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'appserver-template'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures appserver-template'
+long_description 'Installs/Configures appserver-template'
+version          '0.1.0'
+
