ab108c75b6983aad65457e25b05d0f32202f3f8a
Arvid Björkström <arvid@bjorkstrom.se>
2014-08-18 16:48:35 +0200

    Agnoster 2

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,3 +4,10 @@
 #
 
 include_recipe "appserver::default"
+
+template "/home/deploy/.oh-my-zsh/themes/agnoster2.zsh-theme" do
+	source "agnoster2.zsh-theme"
+	owner "deploy"
+	group "deploy"
+	mode "0644"
+end
