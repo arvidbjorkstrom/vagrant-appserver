50285bb03cd9477560097ac80ded902d33449d05
Arvid Björkström <arvid@bjorkstrom.se>
2014-08-19 13:36:23 +0200

    Add custom theme

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,9 +5,14 @@
 
 include_recipe "appserver::default"
 
-template "/home/deploy/.oh-my-zsh/themes/agnoster2.zsh-theme" do
-	source "agnoster2.zsh-theme"
-	owner "deploy"
-	group "deploy"
-	mode "0644"
+search( :users, 'shell:*zsh AND NOT action:remove' ).each do |u|
+  user_id = u["id"]
+
+  template "/home/#{user_id}/.oh-my-zsh/themes/agnoster2.zsh-theme" do
+    source 'agnoster2.zsh-theme'
+    owner 'deploy'
+    group 'deploy'
+    mode '0644'
+    only_if { ::File.directory?("/home/#{user_id}/.oh-my-zsh/themes") }
+  end
 end
