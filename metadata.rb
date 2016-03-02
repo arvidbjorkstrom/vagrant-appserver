298565527f66a01f36dbec06f092c6bafae3ba24
Arvid Björkström <arvid@bjorkstrom.se>
2016-03-02 17:56:41 +0100

    App server cookbook version

--- metadata.rb
+++ metadata.rb
@@ -7,4 +7,4 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 supports 'ubuntu', '>= 12.04.0'
 
-depends 'appserver' # https://github.com/arvidbjorkstrom/chef-appserver
+depends 'appserver', '~>1.2.0' # https://github.com/arvidbjorkstrom/chef-appserver
